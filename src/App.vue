49ac6e71a7f2233e42498ff728aaed616448cf21
Your Name <you@example.com>
2018-11-21 11:59:22 +0800

    'watch-class'

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,17 @@
     <mt-header fixed title="珠峰课堂" class="header"></mt-header>
     <router-view style="margin-top:50px;"></router-view>
     <mt-tabbar v-model="selected">
-      <mt-tab-item id="tab1">
+      <mt-tab-item id="/home" :class="{'is-selected':$route.path.startsWith('/home')}">
         <Icon iconfont="#icon-xingqiu" slot="icon"></Icon>
-        tab1
+        首页
       </mt-tab-item>
-      <mt-tab-item id="tab2">
+      <mt-tab-item id="/lesson" :class="{'is-selected':$route.path.startsWith('/lesson')}">
         <Icon iconfont="#icon-react" slot="icon"></Icon>
-        tab2
+        我的课程
       </mt-tab-item>
-      <mt-tab-item id="tab3">
+      <mt-tab-item id="/profile" :class="{'is-selected':$route.path.startsWith('/profile')}">
          <Icon iconfont="#icon-xiaolian" slot="icon"></Icon>
-        tab3
+        个人中心
       </mt-tab-item>
     </mt-tabbar>
   </div>
@@ -22,7 +22,15 @@
 export default {
   data(){
     return {
-      selected:'tab2'
+      selected:''
+    }
+  },
+  watch:{ // 监控selected属性的变化，变化后跳转路径
+    $route(){ // 当路由变化后把路由的路径 赋予给seleted，selected值改变会触发页面跳转
+      this.selected = this.$route.fullPath;
+    },
+    selected(newVal){
+      this.$router.push(newVal);
     }
   }
 }
